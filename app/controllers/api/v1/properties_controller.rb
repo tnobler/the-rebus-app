62f58e38e73e3fe4ffea212fc315dff145d38b75
Tanner Noble <tannernoble@gmail.com>
2020-06-23 16:27:50 -0500

    Update property actions

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -1,18 +1,61 @@
 class Api::V1::PropertiesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_property, only: [:show, :edit, :update, :destroy]
+
   def index
-    @properties = Property.all
+    @properties = current_user.properties.all
   end
+
   def show
+    if authorized?
+      respond_to do |format|
+        format.json { render :show }
+      end
+    else
+      handle_unauthorized
+    end
   end
+
   def create
+    @property = current_user.properties.build(property_params)
+    if authorized?
+      respond_to do |format|
+        if @property.save
+          format.json { render :show, status: :created, location: api_v1_property_path(@property) }
+        else
+          format.json { render json: @property.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      handle_unauthorized
+    end
   end
+
   def update
   end
+
   def destroy
   end
+
   private
-      def set_property
-          @property = Property.find(params[:id])
+
+    def set_property
+        @property = Property.find(params[:id])
+    end
+
+    def authorized?
+      @property.user == current_user
+    end
+
+    def handle_unauthorized
+      unless authorized? 
+        respond_to do |format|
+          format.json { render :unauthorized, status: 401 }
+        end
       end
+    end
+
+    def property_params
+      params.require(:property).permit(:name, :street_address, :city, :state, :zipcode, :unit_count, :year_built, :ae_flood_zone, :msa, :submarket, :broker, :analysis_year, :asking_price, :price_per_unit, :offer_price, :sales_price, :notes, :status, :fka, :photo)
+    end
 end
