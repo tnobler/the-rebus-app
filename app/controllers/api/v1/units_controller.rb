5d04213d53b18906f6a8af77c06b310bf368d12b
Tanner Noble <tannernoble@gmail.com>
2020-06-30 23:21:02 -0500

    Add unauthorized json builder file and private methods for units_controller.

--- app/controllers/api/v1/units_controller.rb
+++ app/controllers/api/v1/units_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UnitsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_unit, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -21,5 +22,17 @@ class Api::V1::UnitsController < ApplicationController
 
       def set_unit
           @unit = Unit.find(params[:id])
+      end
+
+      def authorized?
+        @unit.property.user == current_user
+      end
+
+      def handle_unauthorized
+        unless authorized?
+          respond_to do |format|
+            format.json { render :unauthorized, status: 401 }
+          end
+        end
       end
 end
